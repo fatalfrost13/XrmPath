83038c254864d7f80c1a2023f1f9aeae868f77e3
Terence Jee <terence.jee@hotmail.com>
2022-08-21 09:41:07 -0600

    Add models

---
+++ XrmPath.Umbraco10Base/XrmPath.Web/Utils/Models/UmbracoCustomFields.cs
@@ -0,0 +1,11 @@
+﻿
+namespace XrmPath.UmbracoUtilities.Models
+{
+    public static class UmbracoCustomFields
+    {
+        public static readonly string BodyText = "bodyText";
+        public static readonly string Hidden = "umbracoNavihide";
+        public static readonly string Title = "title";
+        public static readonly string UrlPicker = "urlPicker";
+    }
+}
