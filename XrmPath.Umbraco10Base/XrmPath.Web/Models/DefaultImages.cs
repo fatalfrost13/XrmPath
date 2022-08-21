885fa6688325d6604b96b4ca3525faf8acae7585
Terence Jee <terence.jee@hotmail.com>
2022-08-20 21:28:37 -0600

    Update templates

---
+++ XrmPath.Umbraco10Base/XrmPath.Web/Models/DefaultImages.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace XrmPath.UmbracoCore.Models
+{
+    public static class DefaultImages
+    {
+        public static readonly string IconPdf = "/images/icons/pdf.png";
+        public static readonly string IconDoc = "/images/icons/doc.png";
+        public static readonly string IconWarning = "/images/icons/warning.png";
+        public static readonly string LoadingImage = "/images/loading.gif";
+    }
+}
