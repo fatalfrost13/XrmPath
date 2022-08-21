d5f602dacb5a7413e72a8a28848735c70e4b3089
Terence Jee <terence.jee@hotmail.com>
2022-08-20 19:02:07 -0600

    .gitignore and add Umbraco.Core library

---
+++ XrmPath.Umbraco10Base/XrmPath.UmbracoCore/Models/UmbracoCustomFields.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace XrmPath.UmbracoCore.Models
+{
+    public static class UmbracoCustomFields
+    {
+        public static readonly string AllowComments = "allowComments";
+        public static readonly string ArticleDate = "articleDate";
+        public static readonly string Body = "body";
+        public static readonly string BodyText = "bodyText";
+        public static readonly string Code = "code";
+        public static readonly string Color = "color";
+        public static readonly string CropperBrightness = "cropperBrightness";
+        public static readonly string CropperContrast = "cropperContrast";
+        public static readonly string CropperSaturation = "cropperSaturation";
+        public static readonly string Date = "date";
+        public static readonly string Description = "description";
+        public static readonly string DisableDropdown = "disableDropdown";
+        public static readonly string EmailSubject = "emailSubject";
+        public static readonly string EmailAdmin = "emailAdmin";
+        public static readonly string EmailFrom = "emailFrom";
+        public static readonly string EmailMessage = "emailMessage";
+        public static readonly string EmailBcc = "emailBcc";
+        public static readonly string FileTextContent = "fileTextContent";
+        public static readonly string FooterLinks = "footerLinks";
+        public static readonly string FooterSocialLinks = "footerSocialLinks";
+        public static readonly string Grid = "grid";
+        public static readonly string Hidden = "umbracoNaviHide";
+        public static readonly string History = "history";
+        public static readonly string Icon = "icon";
+        public static readonly string ImageIcon = "imageIcon";
+        public static readonly string ImageIconStyle = "imageIconStyle";
+        public static readonly string KeywordTags = "keywordTags";
+        public static readonly string LanguageId = "languageId";
+        public static readonly string LatestArticlesSubtitle = "latestArticlesSubtitle";
+        public static readonly string LatestArticlesTitle = "latestArticlesTitle";
+        public static readonly string JsonValue = "jsonValue";
+        public static readonly string MainCarousel = "mainCarousel";
+        public static readonly string MainImage = "mainImage";
+        public static readonly string MetaDescription = "metaDescription";
+        public static readonly string MetaKeywords = "metaKeywords";
+        public static readonly string NodeName = "nodeName";
+        public static readonly string OldUrl = "oldUrl";
+        public static readonly string PageSize = "pageSize";
+        public static readonly string RedirectUrl = "redirectUrl";
+        public static readonly string ReferenceId = "referenceId";
+        public static readonly string RequiresAuthentication = "requiresAuthentication";
+        public static readonly string ShowComments = "showComments";
+        public static readonly string SlideImage = "slideImage";
+        public static readonly string SlideLink = "slideLink";
+        public static readonly string SlideSubtitle = "slideSubtitle";
+        public static readonly string SlideTitle = "slideTitle";
+        public static readonly string SourceNode = "sourceNode";
+        public static readonly string Tags = "tags";
+        public static readonly string TextOnlyInNavigation = "textOnlyInNavigation";
+        public static readonly string Title = "title";
+        public static readonly string UmbracoPageNotFound = "umbracoPageNotFound";
+        public static readonly string UniqueId = "uniqueId";
+        public static readonly string UrlPicker = "urlPicker";
+        public static readonly string UserName = "userName";
+        public static readonly string UserComment = "userComment";
+        public static readonly string VerificationKey = "verificationKey";
+        public static readonly string Weight = "weight";
+    }
+}
---
+++ XrmPath.Umbraco10Base/XrmPath.UmbracoCore/Models/UrlPicker.cs
@@ -0,0 +1,23 @@
+﻿namespace XrmPath.UmbracoCore.Models
+{
+    using Umbraco.Cms.Core.Models;
+
+    public class UrlPicker
+    {
+        public string Title { get; set; } = "";
+        public string Caption { get; set; } = "";
+        public LinkType LinkType { get; set; }  //0=Content, 1=Media, 2=External
+        public int? NodeId { get; set; }
+        public string Url { get; set; } = "";
+        public bool NewWindow { get; set; }
+    }
+
+    public class LinkModel
+    {
+        public string Url { get; set; } = string.Empty;
+        public string Name { get; set; } = "";
+        public int Id { get; set; }
+        public LinkType Type { get; set; } 
+        public string Target { get; set; } = "";
+    }
+}
