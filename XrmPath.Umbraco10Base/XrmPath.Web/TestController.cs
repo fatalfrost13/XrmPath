a1552f5af377f1357673216145af52a8da1a57a2
Terence Jee <terence.jee@hotmail.com>
2022-08-21 22:20:38 -0600

    Create Test API controller example

---
+++ XrmPath.Umbraco10Base/XrmPath.Web/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common;
+
+namespace XrmPath.Web.Controllers
+{
+    [Route("TestController/[action]")]
+    public class TestController : Controller
+    {
+        private readonly UmbracoHelper _umbracoHelper;
+
+        public TestController(UmbracoHelper umbracoHelper) => _umbracoHelper = umbracoHelper;
+
+        public IActionResult GetHomeNodeName()
+        {
+            IPublishedContent? rootNode = _umbracoHelper.ContentAtRoot().FirstOrDefault();
+
+            if (rootNode is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rootNode.Name);
+        }
+    }
+}
