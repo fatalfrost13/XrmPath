bbf856ad6ceb94c352c704381a983307ec2f0a60
Terence Jee <terence.jee@hotmail.com>
2022-08-23 21:29:18 -0600

    Logging works

--- XrmPath.Umbraco10Starter/XrmPath.Web/Controllers/SearchController.cs
+++ XrmPath.Umbraco10Starter/XrmPath.Web/Controllers/SearchController.cs
@@ -4,6 +4,7 @@ using Examine;
 using XrmPath.UmbracoCore.Models;
 using Umbraco.Cms.Core.Services;
 using XrmPath.UmbracoCore.Utilities;
+using XrmPath.UmbracoCore;
 
 namespace XrmPath.Web.Controllers
 {
@@ -14,20 +15,23 @@ namespace XrmPath.Web.Controllers
         private readonly ServiceUtility? _serviceUtil;
         private readonly SearchUtility? _searchUtil;
         private readonly PublishedContentUtility? _pcUtil;
+        private readonly LoggingUtility? _loggingUtil;
         //private readonly UmbracoHelper? _umbracoHelper;
         //private readonly IMediaService? _mediaService;
 
-        public SearchController(ILogger<object> iLogger, UmbracoHelper umbracoHelper, IMediaService mediaService, IExamineManager examineIndex) {
+        public SearchController(ILogger<object> logger, UmbracoHelper umbracoHelper, IMediaService mediaService, IExamineManager examineIndex) {
 
             if (_serviceUtil == null) {
-                _serviceUtil = new ServiceUtility(iLogger, umbracoHelper, mediaService, examineIndex);
+                _serviceUtil = new ServiceUtility(logger, umbracoHelper, mediaService, examineIndex);
                 _pcUtil = _serviceUtil?.GetPublishedContentUtility();
                 _searchUtil = _serviceUtil?.GetSearchUtility();
+                _loggingUtil = _serviceUtil?.GetLoggingUtility();
             }
         }
 
         public SearchResultItemPager? GetSearchResults(string searchterm, int pagesize, int currentpage)
         {
+            //_loggingUtil?.Information("DOES THIS WORK?!?!");
             var results = _searchUtil?.GetSearchResultPager(searchterm, pagesize, currentpage);
             return results;
         }
